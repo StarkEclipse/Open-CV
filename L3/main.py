20552de7a920124c139d7900b71a790cc0254a38
StarkEclipse <nore_nate@icloud.com>
2026-06-12 20:29:24 +0200

    Add files via upload

--- L3/main.py
+++ L3/main.py
@@ -16,14 +16,25 @@ img1 = cv2.imread("/Users/onorenosenathanikhuoria/Desktop/Open CV/L3/deku.jpg")
 # cv2.imshow("Grayscaled with pixel", img1)
 
 ## Rotating Images
-(height, width) = img1.shape[:2]
-center = (width // 2, height // 2)
-angle = 270
-scale = 1.0
-rotation_matrix = cv2.getRotationMatrix2D(center, angle, scale)
-rotatedimg = cv2.warpAffine(img1, rotation_matrix, (width, height))
-cv2.imshow("Og image", img1)
-cv2.imshow("Rotated image", rotatedimg)
+# (height, width) = img1.shape[:2]
+# center = (width // 2, height // 2)
+# angle = 270
+# scale = 1.0
+# rotation_matrix = cv2.getRotationMatrix2D(center, angle, scale)
+# rotatedimg = cv2.warpAffine(img1, rotation_matrix, (width, height))
+# cv2.imshow("Og image", img1)
+# cv2.imshow("Rotated image", rotatedimg)
+
+## Edge detection in an image
+# gray_img = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+# blurred_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
+# edges = cv2.Canny(blurred_img, threshold1=300, threshold2=400)
+# cv2.imshow("Original Image", img1)
+# cv2.imshow("Edge detection", edges)
+
+hsv_img = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
+cv2.imshow("HSV Image", hsv_img)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
