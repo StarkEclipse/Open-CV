d99e01f8ffa1d3d7f9ae07d5cd75bb048b57ab6c
StarkEclipse <nore_nate@icloud.com>
2026-06-30 19:22:41 +0200

    Add files via upload

--- L6/main.py
+++ L6/main.py
@@ -7,6 +7,7 @@ path = "/Users/onorenosenathanikhuoria/Desktop/Open CV/L6/images"
 mean_height = 0
 mean_width = 0
 numofimages = len(os.listdir(path))
+
 for file in os.listdir(path):
     img = Image.open(os.path.join(path, file))
     width, height = (img.size)
@@ -16,6 +17,7 @@ mean_width = mean_width // numofimages
 mean_height = mean_height // numofimages
 print(mean_width)
 print(mean_height)
+
 for file in os.listdir(path):
     if file.endswith(".jpg") or file.endswith(".jpeg)") or file.endswith(".png"):
         img = Image.open(os.path.join(path, file))
@@ -24,3 +26,22 @@ for file in os.listdir(path):
         img_resized = img.resize((mean_width, mean_height,), Image.Resampling.LANCZOS)
         img_resized.save(file, 'JPEG', quality = 95)
         print(img.filename.split('\\')[-1], "is resized")
+
+def videogenerator():
+    video_name = "My Video.avi"
+    os.chdir(path)
+    images = []
+    for img in os.listdir(path):
+        if img.endswith("jpg") or img.endswith("jpeg") or img.endswith("png"):
+            images.append(img)
+            print(images)
+
+    frame = cv2.imread(os.path.join(path, images[0]))
+    height, width, layers = frame.shape
+    video = cv2.VideoWriter(video_name, 0, 1, (width, height))
+    for image in images:
+        video.write(cv2.imread(os.path.join(path, image)))
+    
+    cv2.destroyAllWindows()
+    video.release()
+videogenerator()
